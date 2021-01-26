18537a0e2edde586bba41c32d0e4f095d7eb1f32
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-01-26 10:58:10 +0700

    Update cLogin.php

---
+++ controller/cLogin.php
@@ -0,0 +1,21 @@
+<?php 
+    include_once "model/mLogin.php"; //memanggil mLogin.php
+
+    class cLogin {
+        public $mLogin;
+
+        public function __construct() //konstruktor
+        {
+            $this->mLogin = new mLogin(); //pemanggilan class
+        }
+
+        public function proses(){ //method proses
+            $result = $this->mLogin->getLogin();
+
+            if($result == 'login'){
+                include 'view/vIndex.php';
+            }else{
+                include 'view/vLogin';
+            }
+        }
+    }
