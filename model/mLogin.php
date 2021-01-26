ab0de3edce01ef54ae4cfe5b4625c321ef65c487
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-01-26 12:55:49 +0700

    Update mLogin.php

--- model/mLogin.php
+++ model/mLogin.php
@@ -1,7 +1,13 @@
 <?php
+    session_start();
+
     class mLogin {
         public function getLogin() {
+            include 'koneksi.php';
             if(isset($_REQUEST['username']) && isset($_REQUEST['password'])){
+                $username = $_REQUEST['username'];
+                $Admin = mysqli_query($koneksi,"SELECT * FROM admin WHERE username = '$username'");
+                $dataAdmin = mysqli_fetch_array($Admin);
                 if ($_REQUEST['username']=='admin' && $_REQUEST['password']=='admin'){
                     return 'login';
                 }else{
