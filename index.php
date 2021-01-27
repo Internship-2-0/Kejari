886282333c180f6b54b03528581c9464e64cd228
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-01-27 20:19:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,31 +3,9 @@
 <?php 
 
 	if (isset($_GET['halaman'])) {
-          if ($_GET['halaman'] == 'produk') {
-            include 'pages/produk.php';
-          }
-          else if ($_GET['halaman'] == 'pelanggan') {
-            include 'pages/pelanggan.php';
-          }
-          else if ($_GET['halaman'] == 'pembelian') {
-            include 'pages/pembelian.php';
-          }
-          else if ($_GET['halaman'] == 'login') {
+          if ($_GET['halaman'] == 'login') {
             include 'pages/login.php';
-          }
-          else if ($_GET['halaman'] == 'detail') {
-            include 'pages/detail.php';
-          }
-          else if ($_GET['halaman'] == 'tambah_produk') {
-            include 'pages/tambah_produk.php';
-          }
-          else if ($_GET['halaman'] == 'hapus_produk') {
-            include 'pages/hapus_produk.php';
-          }
-          else if ($_GET['halaman'] == 'ubah_produk') {
-            include 'pages/ubah_produk.php';
-          }
-          else if ($_GET['halaman'] == 'logout') {
+          } else if ($_GET['halaman'] == 'logout') {
             include 'pages/logout.php';
           }
     }
