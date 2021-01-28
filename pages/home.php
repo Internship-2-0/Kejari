00b6eccbb889ed4271e546a440ced107fe42549a
ardiandani <62336429+ardiandani@users.noreply.github.com>
2021-01-28 13:42:04 +0700

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,16 @@
-<h2 class="main text-title">Selamat Datang</h2>
+<div class="ml-4">
+  <h2 class="">Selamat Datang</h2>
+
+  <div class="dropdown">
+    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+      Pilih Surat
+    </button>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Surat Perintah Tugas</a>
+      <a class="dropdown-item" href="#">Link 2</a>
+      <a class="dropdown-item" href="#">Link 3</a>
+      <a class="dropdown-item" href="#">Link 4</a>
+      <a class="dropdown-item" href="#">Link 5</a>
+    </div>
+  </div>
+</div>
