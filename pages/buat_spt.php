add6490cc379f01f1ba9570b742532332893b022
ardiandani <62336429+ardiandani@users.noreply.github.com>
2021-01-28 21:36:19 +0700

    Update buat_spt.php

--- pages/buat_spt.php
+++ pages/buat_spt.php
@@ -1,13 +1,13 @@
-<div class="mt-4 ml-2">
+<div class="mt-4 ml-4">
   <div class="text-center">
     <h4>Form Surat Perintah Tugas</h4>
   </div>
   <div class="form-group">
-    <label for="usr">Name:</label>
-    <input type="text" class="form-control" id="usr">
+    <label for="nomor">Nomor Surat:</label>
+    <input type="text" class="form-control" id="nomor">
   </div>
   <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
+    <label for="menimbang">Menimbang:</label>
+    <input type="text" class="form-control" id="menimbang">
   </div>
 </div>
