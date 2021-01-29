a02dc9291e1e61f487a091cefd0d6c32b92ae13d
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-01-29 08:35:20 +0700

    Update kejari.sql

--- database/kejari.sql
+++ database/kejari.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Jan 2021 pada 02.49
+-- Waktu pembuatan: 29 Jan 2021 pada 02.34
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 8.0.0
 
@@ -39,6 +39,75 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`username`, `password`) VALUES
 ('admin', 'admin');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menimbang_spt`
+--
+
+CREATE TABLE `menimbang_spt` (
+  `id_spt` int(100) NOT NULL,
+  `isi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `nip` varchar(15) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `pangkat` varchar(50) NOT NULL,
+  `jabatan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas_spt`
+--
+
+CREATE TABLE `petugas_spt` (
+  `id_spt` int(100) NOT NULL,
+  `nip` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `spt`
+--
+
+CREATE TABLE `spt` (
+  `id_spt` int(100) NOT NULL,
+  `nomor_surat` varchar(5) NOT NULL,
+  `kode_pejabat` varchar(5) NOT NULL,
+  `kode_masalah` varchar(5) NOT NULL,
+  `tanggal` int(11) NOT NULL,
+  `bulan` int(11) NOT NULL,
+  `tahun` int(11) NOT NULL,
+  `dasar_1` text NOT NULL,
+  `dasar_2` text NOT NULL,
+  `untuk_1` text NOT NULL,
+  `untuk_2` text NOT NULL,
+  `mulai` varchar(20) NOT NULL,
+  `selesai` varchar(20) NOT NULL,
+  `tempat` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tembusan_spt`
+--
+
+CREATE TABLE `tembusan_spt` (
+  `id_spt` int(100) NOT NULL,
+  `nip` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -48,6 +117,72 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indeks untuk tabel `menimbang_spt`
+--
+ALTER TABLE `menimbang_spt`
+  ADD KEY `id_spt` (`id_spt`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD KEY `nip` (`nip`);
+
+--
+-- Indeks untuk tabel `petugas_spt`
+--
+ALTER TABLE `petugas_spt`
+  ADD KEY `id_spt` (`id_spt`),
+  ADD KEY `nip` (`nip`);
+
+--
+-- Indeks untuk tabel `spt`
+--
+ALTER TABLE `spt`
+  ADD PRIMARY KEY (`id_spt`);
+
+--
+-- Indeks untuk tabel `tembusan_spt`
+--
+ALTER TABLE `tembusan_spt`
+  ADD KEY `id_spt` (`id_spt`,`nip`),
+  ADD KEY `nip` (`nip`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `spt`
+--
+ALTER TABLE `spt`
+  MODIFY `id_spt` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `menimbang_spt`
+--
+ALTER TABLE `menimbang_spt`
+  ADD CONSTRAINT `menimbang_spt_ibfk_1` FOREIGN KEY (`id_spt`) REFERENCES `spt` (`id_spt`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `petugas_spt`
+--
+ALTER TABLE `petugas_spt`
+  ADD CONSTRAINT `petugas_spt_ibfk_1` FOREIGN KEY (`id_spt`) REFERENCES `spt` (`id_spt`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `petugas_spt_ibfk_2` FOREIGN KEY (`nip`) REFERENCES `petugas` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tembusan_spt`
+--
+ALTER TABLE `tembusan_spt`
+  ADD CONSTRAINT `tembusan_spt_ibfk_1` FOREIGN KEY (`id_spt`) REFERENCES `spt` (`id_spt`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tembusan_spt_ibfk_2` FOREIGN KEY (`nip`) REFERENCES `petugas` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
