430d1e5282a74509263a7e56c1e7d347a66b457e
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-01-29 08:14:49 +0700

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,11 @@
-<div class="ml-4"></div>
+<div class="ml-4">
 <h2 class="">Selamat Datang</h2>
 
 <div class="dropdown">
   <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
     Pilih Surat
   </button>
-  <div class="dropdown-menu"></div>
+  <div class="dropdown-menu">
   <a class="dropdown-item" href="index.php?halaman=spt">Surat Perintah Tugas</a>
+  </div>
 </div>
