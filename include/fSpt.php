6edb822e0802b689c136241a63252501692350b3
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-01-29 10:04:08 +0700

    Update fSpt.php

--- include/fSpt.php
+++ include/fSpt.php
@@ -1,15 +1,23 @@
 <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
 <script lang="javascript">
-    var idf = 1;
+    //append menimbang
+    var jmlhMenimbang = 1;
     function tambahMenimbang(){
         var stre;
-        stre = "<div id=\"srow" + idf + "\"><input type=\"text\" name=\"menimbang[]\" class=\"form-control\" placeholder=\"menimbang" + idf + "\"/> <button href=\"#\" class=\"btn btn-primary mt-2 mb-2\" onclick=\"hapusElemen(srow" + idf +"); return false;\">Hapus</button></div >";
+        stre = "<div id=\"srow" + jmlhMenimbang + "\"><input type=\"text\" name=\"menimbang[]\" class=\"form-control\" placeholder=\"menimbang " + jmlhMenimbang + "\"/> <button href=\"#\" class=\"btn btn-primary mt-2 mb-2\" onclick=\"hapusElemen(srow" + jmlhMenimbang +",jmlhMenimbang); return false;\">Hapus</button></div >";
         $("#menimbang").append(stre);
-        idf += 1;
+        jmlhMenimbang += 1;
     }
 
-    function hapusElemen(menimbang){
-        $(menimbang).remove();
-        idf -= 1;
+    //append petugas
+    var jmlhPetugas = 1;
+    function tambahPetugas(){
+
+    }
+
+    //hapus elemen/append
+    function hapusElemen(srowKe, total){
+        $(srowKe).remove();
+        total -= 1;
     }
 </script>
