fc7f67a67708e2cfa2892c2968d954a28b3a5886
ardiandani <62336429+ardiandani@users.noreply.github.com>
2021-02-02 12:49:25 +0700

    Update template_spt.php

---
+++ template_surat/template_spt.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <div>
+    <center>
+      <h3>KEJAKSAAN REPUBLIK INDONESIA</h3>
+      <h2>KEJAKSAAN TINGGI JAWA TENGAH</h2>
+      <h1>KEJAKSAAN NEGERI KOTA SEMARANG</h1>
+      <p>
+        Jl. Abdulrahman Saleh No.5-9, Kota Semarang 50145
+      </p>
+      <p>
+        Telp / Fax. (024) 7607507 www.kejari.semarangkota.go.id
+      </p>
+      <hr>
+    </center>
+  </div>
+
+  <div style="text-align: right;">
+    <p>IN.1</p>
+    <p>Copy ke : </p>
+    <p>Dari : </p>
+  </div>
+  <br>
+  <div>
+    <center>
+      <h4>SURAT PERINTAH TUGAS</h4>
+      <p>NOMOR: </p>
+      <h4>KEPALA KEJAKSAAN NEGERI KOTA SEMARANG</h4>
+    </center>
+    <br>
+
+    <div>
+      <div style="margin-left: 2rem;">
+        <p>Menimbang : </p>
+        <p>Dasar : </p>
+      </div>
+      <p><b>
+          <center>MEMERINTAHKAN</center>
+        </b></p>
+      <div style="margin-left: 2rem">
+        <p>Kepada : </p>
+        <br>
+        <p>Untuk : </p>
+      </div>
+    </div>
+  </div>
+  </div>
+  <div style="text-align: right;">
+    <p>Dikeluarkan di : (Kota)</p>
+    <p>Pada tanggal : (Tgl terbit)</p>
+    <p><b>KEPALA KEJAKSAAN NEGERI KOTA SEMARANG,</b></p>
+    <br><br><br>
+    <p><b>SUMURUNG P. SIMAREMARE, S.H., M.H.</b></p>
+    <p><b>JAKSA UTAMA PRATAMA NIP. 197007221993031003</b></p>
+  </div>
+  <br>
+
+  <p>Tembusan : </p>
+
+
+
+</body>
+
+</html>
