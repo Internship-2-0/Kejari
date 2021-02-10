758b5002f6bf1a2c19a68bee81de7703fcf651e4
andrmhndr <66835454+andrmhndr@users.noreply.github.com>
2021-02-10 09:52:20 +0700

    database

--- database/kejari2.sql
+++ database/kejari2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Feb 2021 pada 03.50
+-- Waktu pembuatan: 02 Feb 2021 pada 04.19
 -- Versi server: 10.4.17-MariaDB
--- Versi PHP: 8.0.1
+-- Versi PHP: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -51,21 +51,13 @@ CREATE TABLE `menimbang_spt` (
   `urutan_menimbang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `menimbang_spt`
---
-
-INSERT INTO `menimbang_spt` (`id_spt`, `isi`, `urutan_menimbang`) VALUES
-(18, 'menimbang 1', 6),
-(18, 'menimbang 2', 7);
-
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pegawai`
+-- Struktur dari tabel `petugas`
 --
 
-CREATE TABLE `pegawai` (
+CREATE TABLE `petugas` (
   `nip` varchar(15) NOT NULL,
   `nama` varchar(40) NOT NULL,
   `pangkat` varchar(50) NOT NULL,
@@ -73,10 +65,10 @@ CREATE TABLE `pegawai` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `pegawai`
+-- Dumping data untuk tabel `petugas`
 --
 
-INSERT INTO `pegawai` (`nip`, `nama`, `pangkat`, `jabatan`) VALUES
+INSERT INTO `petugas` (`nip`, `nama`, `pangkat`, `jabatan`) VALUES
 ('12362424652152', 'SOEYANTO SESEREHE', 'CEO', 'Kepala CEO'),
 ('', '', '', '');
 
@@ -91,13 +83,6 @@ CREATE TABLE `petugas_spt` (
   `nip` varchar(20) NOT NULL,
   `urutan_petugas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `petugas_spt`
---
-
-INSERT INTO `petugas_spt` (`id_spt`, `nip`, `urutan_petugas`) VALUES
-(18, '12362424652152', 7);
 
 -- --------------------------------------------------------
 
@@ -119,15 +104,10 @@ CREATE TABLE `spt` (
   `untuk_2` text NOT NULL,
   `mulai` date NOT NULL,
   `selesai` date NOT NULL,
-  `tempat` varchar(20) NOT NULL
+  `tempat` varchar(20) NOT NULL,
+  `nama_instansi` varchar(30) NOT NULL,
+  `pimpinan_instansi` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `spt`
---
-
-INSERT INTO `spt` (`id_spt`, `nomor_surat`, `kode_pejabat`, `kode_masalah`, `penerbitan`, `bulan`, `tahun`, `dasar_1`, `dasar_2`, `untuk_1`, `untuk_2`, `mulai`, `selesai`, `tempat`) VALUES
-(18, '23', '34sfs', '34tg', '2021-02-04', 0, 0, 'dasar 1', 'dasar 2', 'untuk 1', 'untuk 2', '2021-02-11', '2021-02-18', 'Semarang');
 
 -- --------------------------------------------------------
 
@@ -140,13 +120,6 @@ CREATE TABLE `tembusan_spt` (
   `isi` text NOT NULL,
   `urutan_isi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `tembusan_spt`
---
-
-INSERT INTO `tembusan_spt` (`id_spt`, `isi`, `urutan_isi`) VALUES
-(18, 'tembusan 1', 6);
 
 --
 -- Indexes for dumped tables
@@ -166,9 +139,9 @@ ALTER TABLE `menimbang_spt`
   ADD KEY `id_spt` (`id_spt`);
 
 --
--- Indeks untuk tabel `pegawai`
+-- Indeks untuk tabel `petugas`
 --
-ALTER TABLE `pegawai`
+ALTER TABLE `petugas`
   ADD KEY `nip` (`nip`);
 
 --
@@ -200,25 +173,25 @@ ALTER TABLE `tembusan_spt`
 -- AUTO_INCREMENT untuk tabel `menimbang_spt`
 --
 ALTER TABLE `menimbang_spt`
-  MODIFY `urutan_menimbang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `urutan_menimbang` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `petugas_spt`
 --
 ALTER TABLE `petugas_spt`
-  MODIFY `urutan_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `urutan_petugas` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `spt`
 --
 ALTER TABLE `spt`
-  MODIFY `id_spt` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_spt` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `tembusan_spt`
 --
 ALTER TABLE `tembusan_spt`
-  MODIFY `urutan_isi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `urutan_isi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -235,7 +208,7 @@ ALTER TABLE `menimbang_spt`
 --
 ALTER TABLE `petugas_spt`
   ADD CONSTRAINT `petugas_spt_ibfk_1` FOREIGN KEY (`id_spt`) REFERENCES `spt` (`id_spt`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `petugas_spt_ibfk_2` FOREIGN KEY (`nip`) REFERENCES `pegawai` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `petugas_spt_ibfk_2` FOREIGN KEY (`nip`) REFERENCES `petugas` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `tembusan_spt`
