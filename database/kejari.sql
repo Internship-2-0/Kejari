9ee7a69d19d01c8a9de19377369f69af3d3779de
ardiandani <ardiandani04@gmail.com>
2021-02-15 09:55:19 +0700

    database

--- database/kejari.sql
+++ database/kejari.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Feb 2021 pada 03.50
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 8.0.1
+-- Generation Time: Feb 15, 2021 at 03:25 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -33,7 +33,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`username`, `password`) VALUES
@@ -42,7 +42,7 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `menimbang_spt`
+-- Table structure for table `menimbang_spt`
 --
 
 CREATE TABLE `menimbang_spt` (
@@ -52,7 +52,7 @@ CREATE TABLE `menimbang_spt` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `menimbang_spt`
+-- Dumping data for table `menimbang_spt`
 --
 
 INSERT INTO `menimbang_spt` (`id_spt`, `isi`, `urutan_menimbang`) VALUES
@@ -62,7 +62,7 @@ INSERT INTO `menimbang_spt` (`id_spt`, `isi`, `urutan_menimbang`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pegawai`
+-- Table structure for table `pegawai`
 --
 
 CREATE TABLE `pegawai` (
@@ -73,17 +73,19 @@ CREATE TABLE `pegawai` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `pegawai`
+-- Dumping data for table `pegawai`
 --
 
 INSERT INTO `pegawai` (`nip`, `nama`, `pangkat`, `jabatan`) VALUES
 ('12362424652152', 'SOEYANTO SESEREHE', 'CEO', 'Kepala CEO'),
-('', '', '', '');
+('', '', '', ''),
+('budiman', '213124123', '-', 'karyawan'),
+('miftahudin', '12345678', '-', 'OB');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `petugas_spt`
+-- Table structure for table `petugas_spt`
 --
 
 CREATE TABLE `petugas_spt` (
@@ -93,7 +95,7 @@ CREATE TABLE `petugas_spt` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `petugas_spt`
+-- Dumping data for table `petugas_spt`
 --
 
 INSERT INTO `petugas_spt` (`id_spt`, `nip`, `urutan_petugas`) VALUES
@@ -102,7 +104,30 @@ INSERT INTO `petugas_spt` (`id_spt`, `nip`, `urutan_petugas`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `spt`
+-- Table structure for table `pps`
+--
+
+CREATE TABLE `pps` (
+  `id_pps` int(100) NOT NULL,
+  `nomor_surat` varchar(5) NOT NULL,
+  `kode_pejabat` varchar(5) NOT NULL,
+  `kode_masalah` varchar(5) NOT NULL,
+  `penerbitan` date NOT NULL,
+  `bulan` int(11) NOT NULL,
+  `tahun` int(11) NOT NULL,
+  `dasar_1` text NOT NULL,
+  `dasar_2` text NOT NULL,
+  `untuk_1` text NOT NULL,
+  `untuk_2` text NOT NULL,
+  `mulai` date NOT NULL,
+  `seelsai` date NOT NULL,
+  `tempat` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spt`
 --
 
 CREATE TABLE `spt` (
@@ -123,7 +148,7 @@ CREATE TABLE `spt` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `spt`
+-- Dumping data for table `spt`
 --
 
 INSERT INTO `spt` (`id_spt`, `nomor_surat`, `kode_pejabat`, `kode_masalah`, `penerbitan`, `bulan`, `tahun`, `dasar_1`, `dasar_2`, `untuk_1`, `untuk_2`, `mulai`, `selesai`, `tempat`) VALUES
@@ -132,7 +157,7 @@ INSERT INTO `spt` (`id_spt`, `nomor_surat`, `kode_pejabat`, `kode_masalah`, `pen
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tembusan_spt`
+-- Table structure for table `tembusan_spt`
 --
 
 CREATE TABLE `tembusan_spt` (
@@ -142,7 +167,7 @@ CREATE TABLE `tembusan_spt` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tembusan_spt`
+-- Dumping data for table `tembusan_spt`
 --
 
 INSERT INTO `tembusan_spt` (`id_spt`, `isi`, `urutan_isi`) VALUES
@@ -153,26 +178,26 @@ INSERT INTO `tembusan_spt` (`id_spt`, `isi`, `urutan_isi`) VALUES
 --
 
 --
--- Indeks untuk tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`username`);
 
 --
--- Indeks untuk tabel `menimbang_spt`
+-- Indexes for table `menimbang_spt`
 --
 ALTER TABLE `menimbang_spt`
   ADD PRIMARY KEY (`urutan_menimbang`),
   ADD KEY `id_spt` (`id_spt`);
 
 --
--- Indeks untuk tabel `pegawai`
+-- Indexes for table `pegawai`
 --
 ALTER TABLE `pegawai`
   ADD KEY `nip` (`nip`);
 
 --
--- Indeks untuk tabel `petugas_spt`
+-- Indexes for table `petugas_spt`
 --
 ALTER TABLE `petugas_spt`
   ADD PRIMARY KEY (`urutan_petugas`) USING BTREE,
@@ -180,65 +205,77 @@ ALTER TABLE `petugas_spt`
   ADD KEY `nip` (`nip`);
 
 --
--- Indeks untuk tabel `spt`
+-- Indexes for table `pps`
+--
+ALTER TABLE `pps`
+  ADD PRIMARY KEY (`id_pps`);
+
+--
+-- Indexes for table `spt`
 --
 ALTER TABLE `spt`
   ADD PRIMARY KEY (`id_spt`);
 
 --
--- Indeks untuk tabel `tembusan_spt`
+-- Indexes for table `tembusan_spt`
 --
 ALTER TABLE `tembusan_spt`
   ADD PRIMARY KEY (`urutan_isi`) USING BTREE,
   ADD KEY `id_spt` (`id_spt`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `menimbang_spt`
+-- AUTO_INCREMENT for table `menimbang_spt`
 --
 ALTER TABLE `menimbang_spt`
   MODIFY `urutan_menimbang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `petugas_spt`
+-- AUTO_INCREMENT for table `petugas_spt`
 --
 ALTER TABLE `petugas_spt`
   MODIFY `urutan_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `spt`
+-- AUTO_INCREMENT for table `pps`
+--
+ALTER TABLE `pps`
+  MODIFY `id_pps` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `spt`
 --
 ALTER TABLE `spt`
   MODIFY `id_spt` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT untuk tabel `tembusan_spt`
+-- AUTO_INCREMENT for table `tembusan_spt`
 --
 ALTER TABLE `tembusan_spt`
   MODIFY `urutan_isi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `menimbang_spt`
+-- Constraints for table `menimbang_spt`
 --
 ALTER TABLE `menimbang_spt`
   ADD CONSTRAINT `menimbang_spt_ibfk_1` FOREIGN KEY (`id_spt`) REFERENCES `spt` (`id_spt`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `petugas_spt`
+-- Constraints for table `petugas_spt`
 --
 ALTER TABLE `petugas_spt`
   ADD CONSTRAINT `petugas_spt_ibfk_1` FOREIGN KEY (`id_spt`) REFERENCES `spt` (`id_spt`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `petugas_spt_ibfk_2` FOREIGN KEY (`nip`) REFERENCES `pegawai` (`nip`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `tembusan_spt`
+-- Constraints for table `tembusan_spt`
 --
 ALTER TABLE `tembusan_spt`
   ADD CONSTRAINT `tembusan_spt_ibfk_1` FOREIGN KEY (`id_spt`) REFERENCES `spt` (`id_spt`) ON DELETE CASCADE ON UPDATE CASCADE;
